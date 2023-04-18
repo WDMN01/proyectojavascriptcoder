0ce0c09e7b28e990a48869f2785269cbfb1d2365
WDMN01 <wdavidmartinezn@hotmail.com>
2023-04-17 21:24:10 -0500

    carrito terminado

--- js/funcional.js
+++ js/funcional.js
@@ -3,6 +3,7 @@ const items = document.getElementById('items');
 const vercarrito =document.getElementById("vercarrito");
 const suma = document.getElementById("vercarrito");
 
+let prodencarrito;
 let mark = '';
 let int = {};
 let carrito= [];
@@ -29,6 +30,10 @@ let comparacion;
 let sup;
 let eliminar;
 let asumi;
+let repeat;
+let restar;
+
+//let repeatproduct;
 
 const modalcontainer = document.getElementById("modal-container");
 document.addEventListener('DOMContentLoaded', ()=>{
@@ -66,28 +71,61 @@ function generateproducto(id, nombre, imagen, cantidad, precio){
          <img src="${imagen}">
          <h3>Disponible ${cantidad} unidades</h3>
          <p>Precio: ${precio}</p>
-         <button class= "btn-dark" id="${id}" onclick="agregaralcarrito('${imagen}','${nombre}','${precio}')" >Agregar al carrito</button>
+         <button class= "btn-dark" id="${id}" onclick="agregaralcarrito('${imagen}','${nombre}','${precio}','${cantidad}')" >Agregar al carrito</button>
       </div>
    `;
 }
 recupercarro=  JSON.parse(localStorage.getItem('carroalmacenado'));
-   
-function agregaralcarrito(imagen, nombre, precio){
+ 
 
-   carrocompra.unshift({imagen,nombre, precio});
+
+function agregaralcarrito( imagen, nombre, precio, cantidad){
+   
+      
+   console.log(nombre)
+   repeat = carrocompra.some((repeatproduct) => repeatproduct.nombre == nombre)
+   console.log(repeat)
+   if(repeat ){
+      
+      carrocompra.map((pro) =>{
+      if(pro.nombre === nombre){
+
+         pro.cantidad++;
+         console.log(pro.cantidad)        
+      }   
+      
+   } );
+   }else{
+      carrocompra.unshift({ imagen,nombre, precio, cantidad});
+   }
+
+   //carrocompra.forEach(mnb => {
+   //  }
+   //);
+   
+      //console.log(elemento)
+   
+    
+
+   
+   
+   
    
    
    let carritomarkup;
-   function generateprodcarrito(imagen, nombre, precio){
-      
+   function generateprodcarrito(imagen, nombre, precio, cantidad){
       
    }
    comparacion= carrocompra;
-   for (let prodencarrito of carrocompra){
-      carritomarkup += generateprodcarrito(prodencarrito.imagen,prodencarrito.nombre, prodencarrito.precio0);
+   for ( prodencarrito of carrocompra){
+      carritomarkup += generateprodcarrito(prodencarrito.imagen,prodencarrito.nombre, prodencarrito.precio, prodencarrito.cantidad );
       inte= prodencarrito.nombre
+   
+   
    }
    
+
+
 
    console.log(carrocompra)
    carroalamacenado = localStorage.setItem('carroalmacenado',JSON.stringify(carrocompra))
@@ -149,9 +187,10 @@ const dibujarcarrito = () =>{
       carritocontent= document.createElement("div");
       carritocontent.className = "modal-content";
       carritocontent.innerHTML = `
-      <button class="menos"><img src="../imagenes/menos.png" id="bmenos"></button>
-      <h3 class="cantidade">Cantidad<br>${cantidadelem}</h3>
-      <button class="mas"><img src="../imagenes/mas.png" id="bmas"></button>
+      <button class="menos" onclick="restprod('${el.nombre}', '${el.cantidad}')"><img src="../imagenes/menos.png" id="bmenos"></button>
+      <h3 class="cantidade">Cantidad<br>${el.cantidad}</h3>
+      <button class="mas" onclick="sumarprod('${el.nombre}', '${el.cantidad}')"><img src="../imagenes/mas.png" id="bmas"></button>
+      <p class="totalprodcarr">Total: ${el.cantidad * el.precio}</>
       <img class="imagencarrito" src="${el.imagen}">
       <h1 class="nomprod">${el.nombre}</h1>
       <p class="preprod">${el.precio} </p>
@@ -179,11 +218,13 @@ const dibujarcarrito = () =>{
       
       
       //eliminar.addEventListener("click", eliminarproducto);
+
+      
       
    };
 
 
-   const total = recupercarro.reduce((acumula, el) => acumula + parseInt(el.precio, 10), 0);
+   const total = recupercarro.reduce((acumula, el) => acumula + parseInt(el.precio, 10)* parseInt(el.cantidad, 10), 0);
    const totalcompra = document.createElement("div")
    totalcompra.className ="tota-compra"
    totalcompra.innerHTML = `Total a pagar: ${total}`;
@@ -215,7 +256,7 @@ const eliminarproducto = (mm) =>{
    cantidadcarro.innerHTML = "";
    cantidadcarro.innerHTML = cantidadtot ;
    console.log(filtro);
-    
+   //recupercarro =filtro 
    localStorage.clear();
    carroalamacenado = localStorage.setItem('carroalmacenado',JSON.stringify(filtro))
    console.log(recupercarro)
@@ -224,6 +265,39 @@ const eliminarproducto = (mm) =>{
    
    
 };
+
+const sumarprod = (nombre) =>{
+   carrocompra.map((pro) =>{
+      if(pro.nombre === nombre){
+
+         pro.cantidad++;
+         console.log(pro) 
+         localStorage.clear()
+         carroalamacenado = localStorage.setItem('carroalmacenado',JSON.stringify(carrocompra))     
+         dibujarcarrito();   
+      }   
+        
+      
+   });
+     
+}
+const restprod = (nombre) =>{
+   carrocompra.map((pro) =>{
+      if(pro.nombre === nombre && pro.cantidad >0){
+
+         pro.cantidad--;
+         console.log(pro) 
+         localStorage.clear()
+         carroalamacenado = localStorage.setItem('carroalmacenado',JSON.stringify(carrocompra))     
+         dibujarcarrito();   
+      }   
+        
+      
+   });
+     
+}
+
+
 
 vercarrito.addEventListener("click", dibujarcarrito);
 
